74c8b4220aebde0e0df70494cc48ddc8dc3a2bc3
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-11 14:28:36 +0530

    ✅ Divide Array Into Equal Pairs | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 17.2 MB | 2026-07-11

--- leetcode/easy/2206-divide-array-into-equal-pairs/solution.cpp
+++ leetcode/easy/2206-divide-array-into-equal-pairs/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool divideArray(vector<int>& nums) {
         int n=nums.size();
+        /*
         unordered_map<int,int> hashMap;
 
         for(auto& it : nums){
@@ -10,6 +11,19 @@ public:
 
         for(auto& it : hashMap){
             if(it.second % 2 != 0){
+                return false;
+            }
+        }
+        return true;
+        */
+
+        // Since constraints are 500 we can use frequency array
+        int frequency[501]={0};
+        for(int i=0;i<n;i++){
+            frequency[nums[i]]++;
+        }
+        for(int i=1;i<=500;i++){
+            if(frequency[i]%2 != 0){
                 return false;
             }
         }
