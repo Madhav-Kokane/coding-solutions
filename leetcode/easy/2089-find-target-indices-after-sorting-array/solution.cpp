1fcad46446f2c4bc433b2a61f5b422ef99fc9a7b
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-12 16:51:55 +0530

    ✅ Find Target Indices After Sorting Array | Easy | C++ | Runtime: 3 ms (10.35%) | Memory: 15.1 MB | 2026-07-12

---
+++ leetcode/easy/2089-find-target-indices-after-sorting-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> result;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i] == target){
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
