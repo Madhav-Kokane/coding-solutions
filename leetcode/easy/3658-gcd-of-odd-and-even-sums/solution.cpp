56328145e01368d346242b0772251918416c1983
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-15 11:58:44 +0530

    ✅ GCD of Odd and Even Sums | Easy | C++ | Runtime: 3 ms (16.66%) | Memory: 8.6 MB | 2026-07-15

--- leetcode/easy/3658-gcd-of-odd-and-even-sums/solution.cpp
+++ leetcode/easy/3658-gcd-of-odd-and-even-sums/solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int gcd(int x,int y){
+        while(y!=0){
+            int rem=x%y;
+            x=y;
+            y=rem;
+        }
+        return x;
+    }
     int gcdOfOddEvenSums(int n) {
-        return n;
+        // return n;
+        int sumOdd=(n*n);
+        int sumEven=n*(n+1);
+        if(sumOdd>sumEven){
+            return gcd(sumOdd,sumEven);
+        }
+        return gcd(sumEven,sumOdd);
     }
 };
