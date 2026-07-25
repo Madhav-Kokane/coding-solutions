92880ae5c70a0200696bd6b0f9d95807d0c5a80f
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-25 14:24:14 +0530

    ✅ Maximum Product of Two Digits | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 9.5 MB | 2026-07-25

--- leetcode/easy/3536-maximum-product-of-two-digits/solution.cpp
+++ leetcode/easy/3536-maximum-product-of-two-digits/solution.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        string str=to_string(n);
+        /*
         vector<int> nums;
-
-
-        
         while(n>9){
             int temp=n%10;
             nums.push_back(temp);
@@ -16,5 +13,11 @@ public:
         sort(nums.begin(),nums.end());
         int len=nums.size();
         return nums[len-1]*nums[len-2];
+*/
+        string str=to_string(n);
+        int len1=str.size();
+        sort(str.begin(),str.end());
+        return (str[len1-1]-'0')*(str[len1-2]-'0');
+
     }
 };
