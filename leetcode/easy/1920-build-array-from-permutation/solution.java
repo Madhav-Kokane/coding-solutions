296f4b86bfa8dd0ec446a38c10f317a7116823b2
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-25 16:08:14 +0530

    ✅ Build Array from Permutation | Easy | Java | Runtime: 1 ms (99.49%) | Memory: 47.1 MB | 2026-07-25

---
+++ leetcode/easy/1920-build-array-from-permutation/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int len=nums.length;
+        int[] result=new int[len];
+
+        for(int i=0;i<len;i++){
+            result[i]=nums[nums[i]];
+        }
+        return result;
+    }
+}
