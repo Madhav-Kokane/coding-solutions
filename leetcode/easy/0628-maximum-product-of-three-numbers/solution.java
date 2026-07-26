f47bc5d6a5e2d619e5cb2739ad51cb078dd61104
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-07-26 15:06:37 +0530

    ✅ Maximum Product of Three Numbers | Easy | Java | Runtime: 15 ms (80.39%) | Memory: 48.1 MB | 2026-07-26

---
+++ leetcode/easy/0628-maximum-product-of-three-numbers/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int len=nums.length;
+        if(nums[0] > 0){
+            return nums[len-1]*nums[len-2]*nums[len-3];
+        }
+        else if(nums[len-1] < 0){
+            return nums[len-1]*nums[len-2]*nums[len-3];
+        }else if(nums[0]*nums[1] > nums[len-2]*nums[len-3]){
+            return nums[0]*nums[1]*nums[len-1];
+        }else{
+            return nums[len-1]*nums[len-2]*nums[len-3];
+        }
+        
+    }
+}
