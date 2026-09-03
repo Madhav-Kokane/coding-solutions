40f2b9bf8e8eac513f6ded4837723c2647ed5b55
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-09-03 16:54:27 +0530

    ✅ Missing Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 21.8 MB | 2026-09-03

--- leetcode/easy/0268-missing-number/solution.cpp
+++ leetcode/easy/0268-missing-number/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        /*
         unordered_set<int> st;
         for(auto it : nums){
             st.insert({it});
@@ -9,6 +10,20 @@ public:
         int n=nums.size();
         for(int i=0;i<=n;i++){
             if(st.find(i) == st.end()){
+                return i;
+            }
+        }
+        return -1;
+        */
+
+        int n=nums.size();
+        vector<bool> present(n+1,false);
+        for(auto it : nums){
+            present[it]=true;
+        }
+
+        for(int i=0;i<present.size();i++){
+            if(present[i] == false){
                 return i;
             }
         }
