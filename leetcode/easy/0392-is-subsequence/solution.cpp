f5af4d454e8b5dd50b7f53c7d2ee008de31c71b0
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-09-14 15:05:53 +0530

    ✅ Is Subsequence | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.4 MB | 2026-09-14

---
+++ leetcode/easy/0392-is-subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sSize=s.length();
+        int tSize=t.length();
+
+        int back=0;
+        for(int i=0;i<tSize;i++){
+            if(s[back] == t[i]){
+                back++;
+            }
+        }
+        
+        if(back>=sSize){
+            return true;
+        }
+        return false;
+    }
+};
