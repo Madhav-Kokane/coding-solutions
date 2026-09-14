06a54c3ca043c1ea0882547e852c82e91ad01ef3
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-09-14 14:18:49 +0530

    ✅ Contains Duplicate | Easy | C++ | Runtime: 72 ms (53.12%) | Memory: 111.3 MB | 2026-09-14

--- leetcode/easy/0217-contains-duplicate/solution.cpp
+++ leetcode/easy/0217-contains-duplicate/solution.cpp
@@ -1,38 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        /*
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            if(nums[i] == nums[i+1]){
-                return true;
-            }
-        }
-        return false;
-        */
-        
-        /*
-        unordered_map<int,int> hashMap;
-        for(int i=0;i<n;i++){
-            hashMap[nums[i]]++;
-        }
-
-        for(auto & it : hashMap){
-            if(it.second > 1){
-                return true;
-            }
-        }
-        return false;
-        */
-
-        unordered_set<int> st;
+        unordered_set<int> hashSet;
         for(auto it : nums){
-            if(st.find(it) != st.end()){
+            if(hashSet.find(it) != hashSet.end()){
                 return true;
-            }else{
-                st.insert({it});
             }
+            hashSet.insert({it});
         }
         return false;
     }
