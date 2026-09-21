d1c9cd6da06d6cf50625a4d645d16b9e134a3f03
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-09-21 18:20:36 +0530

    ✅ Binary Search | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 31.2 MB | 2026-09-21

--- leetcode/easy/0704-binary-search/solution.cpp
+++ leetcode/easy/0704-binary-search/solution.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start<=end){
-            int mid=(start+end)/2;
+    int binarySearch(int start,int end,vector<int>& nums,int target){
 
-            if(nums[mid] > target){
-                end=mid-1;
-            }else if(nums[mid] < target){
-                start=mid+1;
-            }else{
-                return mid;
-            }
+        if(start>end){
+            return -1;
         }
+
+        int mid=start+(end-start)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+
+        if(nums[mid] > target){
+            return binarySearch(start,mid-1,nums,target);
+        }
+
+        if(nums[mid] < target){
+            return binarySearch(mid+1,end,nums,target);
+        }
+
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        return binarySearch(0,n-1,nums,target);
     }
 };
