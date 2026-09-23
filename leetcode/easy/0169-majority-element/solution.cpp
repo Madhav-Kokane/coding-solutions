14411b4829071767da04d88ecbaff5b8470cd759
Madhav-Kokane <kokanemadhav4@gmail.com>
2026-09-23 14:02:27 +0530

    ✅ Majority Element | Easy | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-09-23

---
+++ leetcode/easy/0169-majority-element/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int ele=nums[0];
+        int count=0;
+
+        for(int i=1;i<n;i++){
+            if(count==0){
+                ele=nums[i];
+            }else if(nums[i] == ele){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return ele;
+    }
+};
